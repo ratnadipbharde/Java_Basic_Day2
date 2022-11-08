80e0e3d99526a3a89b24596029a774839918fdad
ratnadipbharde <ratnadipbharde@gmail.com>
2022-11-08 21:41:55 +0530

    Added Forloop Prog1 Sum Of Natural Number

--- day2/src/main/java/org/example/Main.java
+++ day2/src/main/java/org/example/Main.java
@@ -6,7 +6,9 @@ public class Main {
     public static void main(String[] args) {
 
        IfElse obj1=new IfElse();
-       obj1.digitToWord();
-       obj1.displayUnit();
+      // obj1.digitToWord();
+      // obj1.displayUnit();
+       ForLoop obj2=new ForLoop();
+       obj2.naturalNumber();
     }
 }
